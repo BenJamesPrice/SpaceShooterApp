02878406bbc53fdf3cb7f7e59b2e0c1a05296497
BenJamesPrice <benjprice99@gmail.com>
2018-09-23 14:42:02 +0100

    Added pause feature

--- core/src/com/benprice/spaceshooter/GameScreen.java
+++ core/src/com/benprice/spaceshooter/GameScreen.java
@@ -23,6 +23,8 @@ public class GameScreen implements Screen {
     private Texture laserImage;
     private Texture backgroundImage1;
     private Texture backgroundImage2;
+    private Texture pauseImage;
+    private Texture playImage;
     private float xMax, xCoordBg1, xCoordBg2;
     private Rectangle ship;
     private long lastEnemySpawnTime; //Can be ship or asteroid
@@ -38,16 +40,26 @@ public class GameScreen implements Screen {
     private Array<Rectangle> asteroids; //Add asteroids after core features work
     private Texture asteroidImage;
     private long lastAsteroidSpawnTime;
+    private Rectangle pauseButton;
+    private enum State {
+        PAUSE,
+        RUN
+    }
+    private State state;
+
 
     public GameScreen(final SpaceShooter game) {
         this.game = game;
         game.setPoints(0);
+        this.state = State.RUN;
         // Load textures
         shipImage = new Texture(Gdx.files.internal("ship2.png"));
         enemyImage = new Texture(Gdx.files.internal("enemy.png"));
         laserImage = new Texture(Gdx.files.internal("laser.png"));
         powerUpImage = new Texture(Gdx.files.internal("powerup.png"));
         asteroidImage = new Texture(Gdx.files.internal("asteroid.png"));
+        pauseImage = new Texture(Gdx.files.internal("pause.png"));
+        playImage = new Texture(Gdx.files.internal("play.png"));
 
         // Background
         backgroundImage1 = new Texture(Gdx.files.internal("background.png"));
@@ -62,6 +74,13 @@ public class GameScreen implements Screen {
         // create the camera and the SpriteBatch
         camera = new OrthographicCamera();
         camera.setToOrtho(false, game.screenWidth, game.screenHeight);
+
+        // Create pause/play button
+        pauseButton = new Rectangle();
+        pauseButton.x = 740;
+        pauseButton.y = 420;
+        pauseButton.width = 50;
+        pauseButton.height = 50;
 
         // create a Rectangle to logically represent the ship
         ship = new Rectangle();
@@ -139,141 +158,178 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Clear screen and set background colour
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        switch (state) {
+            case RUN:
+                // Clear screen and set background colour
+                Gdx.gl.glClearColor(0, 0, 0, 1);
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+                camera.update();
+                game.batch.setProjectionMatrix(camera.combined);
 
-        // Move scrolling background
-        xCoordBg1 -= 100 * Gdx.graphics.getDeltaTime();
-        xCoordBg2 = xCoordBg1 + xMax;
-        if (xCoordBg1 <= -xMax) {
-            xCoordBg1 = 0;
-            xCoordBg2 = xMax;
-        }
-
-        // Draw ship, all enemies and lasers
-        game.batch.begin();
-        game.batch.draw(backgroundImage1, xCoordBg1, 0);
-        game.batch.draw(backgroundImage2, xCoordBg2, 0);
-        game.font.draw(game.batch, "Points: " + game.getPoints(), 20, 450);
-        game.batch.draw(shipImage, ship.x, ship.y);
-        for (Rectangle enemy : enemies)
-            game.batch.draw(enemyImage, enemy.x, enemy.y);
-        for (Rectangle laser : lasers)
-            game.batch.draw(laserImage, laser.x, laser.y);
-        for (Rectangle rock:asteroids)
-            game.batch.draw(asteroidImage, rock.x, rock.y);
-        game.batch.draw(powerUpImage, powerUp.x, powerUp.y);
-        game.batch.end();
-
-        // Handle user input
-        if (Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            if (touchPos.y<ship.y+32) ship.y -=5;
-            if (touchPos.y>ship.y+32) ship.y +=5;
-        }
-
-        // Keep ship on screen
-        if (ship.y < 0) ship.y = 0;
-        if (ship.y > game.screenHeight - 64) ship.y = game.screenHeight - 64;
-
-        // Create new enemy and asteroid
-        if (TimeUtils.nanoTime() - lastEnemySpawnTime > 800000000)
-            spawnEnemyShip();
-        if (TimeUtils.millis() - lastAsteroidSpawnTime > 3000)
-            spawnAsteroid();
-
-        // Increase enemy speed
-        int enemySpeed = 250 + (game.getPoints() / 1000) * 30;
-
-        // Move enemy ships
-        Iterator<Rectangle> i = enemies.iterator();
-        while (i.hasNext()) {
-            Rectangle enemy = i.next();
-            enemy.x -= enemySpeed * Gdx.graphics.getDeltaTime();
-            if (enemy.x + 64 < 0)
-                i.remove();
-            if (enemy.overlaps(ship)) {
-                gameOver();
-            }
-        }
-
-        // Move asteroids
-        Iterator<Rectangle> a = asteroids.iterator();
-        while (a.hasNext()) {
-            Rectangle rock = a.next();
-            rock.x -= 200 * Gdx.graphics.getDeltaTime();
-            if (rock.x + 98 < 0)
-                a.remove();
-            if (rock.overlaps(ship)) {
-                gameOver();
-            }
-        }
-
-        // Create powerup
-        if (TimeUtils.millis() - lastPowerUpTime > 20000)
-            spawnPowerUp();
-
-        // Move powerup and handle powerup detection
-        if (powerUpVisible) {
-            powerUp.x -= 500 * Gdx.graphics.getDeltaTime();
-            if (ship.overlaps(powerUp)) {
-                powerUp.x = game.screenWidth;
-                powerUpVisible = false;
-                powerUpActive = true;
-                powerUpActiveTime = TimeUtils.millis();
-            }
-            if (powerUp.x + 34 < 0) {
-                powerUp.x = game.screenWidth;
-                powerUpVisible = false;
-            }
-        }
-
-        // Create new laser
-        if (powerUpActive) {
-            if (TimeUtils.nanoTime() - lastLaserSpawnTime > 200000000)
-                spawnLaser();
-        } else {
-            if (TimeUtils.nanoTime() - lastLaserSpawnTime > 700000000)
-                spawnLaser();
-        }
-
-        // Remove powerup after 5 seconds of it being active
-        if (powerUpActive && TimeUtils.millis() - powerUpActiveTime > 5000) {
-            powerUpActive = false;
-        }
-
-        // Move lasers
-        i = lasers.iterator();
-        while (i.hasNext()) i.next().x += 900 * Gdx.graphics.getDeltaTime();
-        for (Rectangle laser:lasers) {
-            if (laser.x > game.screenWidth) {
-                lasers.removeValue(laser, true);
-                continue;
-            }
-            // Detect hit on enemy
-            for (Rectangle enemy : enemies) {
-                if (laser.overlaps(enemy)) {
-                    enemies.removeValue(enemy, true);
-                    game.setPoints(game.getPoints() + 100);
-                    lasers.removeValue(laser, true);
-                    break;
+                // Move scrolling background
+                xCoordBg1 -= 100 * Gdx.graphics.getDeltaTime();
+                xCoordBg2 = xCoordBg1 + xMax;
+                if (xCoordBg1 <= -xMax) {
+                    xCoordBg1 = 0;
+                    xCoordBg2 = xMax;
                 }
-            }
-            // Detect hit on asteroid
-            for (Rectangle rock : asteroids) {
-                if (laser.overlaps(rock)) {
-                    asteroids.removeValue(rock, true);
-                    game.setPoints(game.getPoints() + 50);
-                    lasers.removeValue(laser, true);
-                    break;
+
+                // Draw ship, all enemies and lasers
+                game.batch.begin();
+                game.batch.draw(backgroundImage1, xCoordBg1, 0);
+                game.batch.draw(backgroundImage2, xCoordBg2, 0);
+                game.font.draw(game.batch, "Points: " + game.getPoints(), 20, 450);
+                game.batch.draw(shipImage, ship.x, ship.y);
+                for (Rectangle enemy : enemies)
+                    game.batch.draw(enemyImage, enemy.x, enemy.y);
+                for (Rectangle laser : lasers)
+                    game.batch.draw(laserImage, laser.x, laser.y);
+                for (Rectangle rock:asteroids)
+                    game.batch.draw(asteroidImage, rock.x, rock.y);
+                game.batch.draw(powerUpImage, powerUp.x, powerUp.y);
+                game.batch.draw(pauseImage, pauseButton.x, pauseButton.y);
+                game.batch.end();
+
+                // Handle user input
+                if (Gdx.input.isTouched()) {
+                    Vector3 touchPos = new Vector3();
+                    touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+                    camera.unproject(touchPos);
+                    if (touchPos.y<ship.y+32) ship.y -=5;
+                    if (touchPos.y>ship.y+32) ship.y +=5;
+                    if(pauseButton.contains(touchPos.x, touchPos.y)) {
+                        state = State.PAUSE;
+                    }
                 }
-            }
+
+                // Keep ship on screen
+                if (ship.y < 0) ship.y = 0;
+                if (ship.y > game.screenHeight - 64) ship.y = game.screenHeight - 64;
+
+                // Create new enemy and asteroid
+                if (TimeUtils.nanoTime() - lastEnemySpawnTime > 800000000)
+                    spawnEnemyShip();
+                if (TimeUtils.millis() - lastAsteroidSpawnTime > 3000)
+                    spawnAsteroid();
+
+                // Increase enemy speed
+                int enemySpeed = 250 + (game.getPoints() / 1000) * 30;
+
+                // Move enemy ships
+                Iterator<Rectangle> i = enemies.iterator();
+                while (i.hasNext()) {
+                    Rectangle enemy = i.next();
+                    enemy.x -= enemySpeed * Gdx.graphics.getDeltaTime();
+                    if (enemy.x + 64 < 0)
+                        i.remove();
+                    if (enemy.overlaps(ship)) {
+                        gameOver();
+                    }
+                }
+
+                // Move asteroids
+                Iterator<Rectangle> a = asteroids.iterator();
+                while (a.hasNext()) {
+                    Rectangle rock = a.next();
+                    rock.x -= 200 * Gdx.graphics.getDeltaTime();
+                    if (rock.x + 98 < 0)
+                        a.remove();
+                    if (rock.overlaps(ship)) {
+                        gameOver();
+                    }
+                }
+
+                // Create powerup
+                if (TimeUtils.millis() - lastPowerUpTime > 20000)
+                    spawnPowerUp();
+
+                // Move powerup and handle powerup detection
+                if (powerUpVisible) {
+                    powerUp.x -= 500 * Gdx.graphics.getDeltaTime();
+                    if (ship.overlaps(powerUp)) {
+                        powerUp.x = game.screenWidth;
+                        powerUpVisible = false;
+                        powerUpActive = true;
+                        powerUpActiveTime = TimeUtils.millis();
+                    }
+                    if (powerUp.x + 34 < 0) {
+                        powerUp.x = game.screenWidth;
+                        powerUpVisible = false;
+                    }
+                }
+
+                // Create new laser
+                if (powerUpActive) {
+                    if (TimeUtils.nanoTime() - lastLaserSpawnTime > 200000000)
+                        spawnLaser();
+                } else {
+                    if (TimeUtils.nanoTime() - lastLaserSpawnTime > 700000000)
+                        spawnLaser();
+                }
+
+                // Remove powerup after 5 seconds of it being active
+                if (powerUpActive && TimeUtils.millis() - powerUpActiveTime > 5000) {
+                    powerUpActive = false;
+                }
+
+                // Move lasers
+                i = lasers.iterator();
+                while (i.hasNext()) i.next().x += 900 * Gdx.graphics.getDeltaTime();
+                for (Rectangle laser:lasers) {
+                    if (laser.x > game.screenWidth) {
+                        lasers.removeValue(laser, true);
+                        continue;
+                    }
+                    // Detect hit on enemy
+                    for (Rectangle enemy : enemies) {
+                        if (laser.overlaps(enemy)) {
+                            enemies.removeValue(enemy, true);
+                            game.setPoints(game.getPoints() + 100);
+                            lasers.removeValue(laser, true);
+                            break;
+                        }
+                    }
+                    // Detect hit on asteroid
+                    for (Rectangle rock : asteroids) {
+                        if (laser.overlaps(rock)) {
+                            asteroids.removeValue(rock, true);
+                            game.setPoints(game.getPoints() + 50);
+                            lasers.removeValue(laser, true);
+                            break;
+                        }
+                    }
+                }
+                break;
+            case PAUSE:
+                // Draw ship, all enemies and lasers
+                game.batch.begin();
+                game.batch.draw(backgroundImage1, xCoordBg1, 0);
+                game.batch.draw(backgroundImage2, xCoordBg2, 0);
+                game.font.draw(game.batch, "Points: " + game.getPoints(), 20, 450);
+                game.batch.draw(shipImage, ship.x, ship.y);
+                for (Rectangle enemy : enemies)
+                    game.batch.draw(enemyImage, enemy.x, enemy.y);
+                for (Rectangle laser : lasers)
+                    game.batch.draw(laserImage, laser.x, laser.y);
+                for (Rectangle rock:asteroids)
+                    game.batch.draw(asteroidImage, rock.x, rock.y);
+                game.batch.draw(powerUpImage, powerUp.x, powerUp.y);
+                game.batch.draw(playImage, pauseButton.x, pauseButton.y);
+                game.batch.end();
+
+                // Handle user input
+                if (Gdx.input.isTouched()) {
+                    Vector3 touchPos = new Vector3();
+                    touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+                    camera.unproject(touchPos);
+                    if(pauseButton.contains(touchPos.x, touchPos.y)) {
+                        state = State.RUN;
+                    }
+                }
+                break;
+            default:
+                break;
         }
     }
 
@@ -284,12 +340,12 @@ public class GameScreen implements Screen {
 
     @Override
     public void pause() {
-
+        this.state = State.PAUSE;
     }
 
     @Override
     public void resume() {
-
+        this.state = State.RUN;
     }
 
     @Override
@@ -307,5 +363,7 @@ public class GameScreen implements Screen {
         powerUpImage.dispose();
         laserSound.dispose();
         asteroidImage.dispose();
+        pauseImage.dispose();
+        playImage.dispose();
     }
 }
